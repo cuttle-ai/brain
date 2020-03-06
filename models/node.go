8ae4fd7693f6c6099f7187f9d5b6cbe2c87739bb
Melvin Davis <melvinodsa@gmail.com>
2020-03-06 09:28:11 +0530

    Node to column node transformations

--- models/node.go
+++ models/node.go
@@ -14,6 +14,44 @@ import (
  * This file contains the model implementation of node's db model
  */
 
+const (
+	//NodeMetadataPropWord is the metadata property of a node for word
+	NodeMetadataPropWord = "Word"
+	//NodeMetadataPropName is the metadata property of a node for name
+	NodeMetadataPropName = "Name"
+	//NodeMetadataPropDimension is the metadata property of a node for dimension
+	NodeMetadataDimension = "Dimension"
+	//NodeMetadataPropMeasure is the metadata property of a node for measure
+	NodeMetadataMeasure = "Measure"
+	//NodeMetadataPropAggregationFn is the metadata property of a node for aggregation function
+	NodeMetadataAggregationFn = "AggregationFn"
+	//NodeMetadataPropDataType is the metadata property of a node for data type
+	NodeMetadataDataType = "DataType"
+)
+
+const (
+	//NodeMetadataPropValueTrue is the value to be put for true as metadata value
+	NodeMetadataPropValueTrue = "true"
+	//NodeMetadataPropValueFalse is the value to be put for false as metadata value
+	NodeMetadataPropValueFalse = "false"
+)
+
+var (
+	//NodeMetadataAggregationFns is the map containing the supported aggregation functions
+	NodeMetadataAggregationFns = map[string]struct{}{
+		interpreter.AggregationFnAvg:   struct{}{},
+		interpreter.AggregationFnCount: struct{}{},
+		interpreter.AggregationFnSum:   struct{}{},
+	}
+	//NodeMetadataDataTypes is the map containing the supported datatypes
+	NodeMetadataDataTypes = map[string]struct{}{
+		interpreter.DataTypeDate:   struct{}{},
+		interpreter.DataTypeFloat:  struct{}{},
+		interpreter.DataTypeInt:    struct{}{},
+		interpreter.DataTypeString: struct{}{},
+	}
+)
+
 //Node represents a octopus node's db record
 type Node struct {
 	gorm.Model
@@ -21,8 +59,8 @@ type Node struct {
 	UID uuid.UUID
 	//Type of the node
 	Type interpreter.Type
-	//PID is the id of the parent of the node
-	PID uint
+	//PUID is the unique id of the parent node
+	PUID uuid.UUID
 	//DatasetID is the id of the dataset to which the node belongs to
 	DatasetID uint
 	//Metadata holds the metadata corresponding to the node
@@ -42,10 +80,80 @@ type NodeMetadata struct {
 
 //ColumnNode returns column node converted form of the node
 func (n Node) ColumnNode() interpreter.ColumnNode {
-	return interpreter.ColumnNode{}
+	dT := interpreter.DataTypeString
+	aggFn := interpreter.AggregationFnCount
+	mes := false
+	dim := false
+	name := ""
+	word := ""
+	for _, v := range n.Metadata {
+		if v.Prop == NodeMetadataPropWord {
+			word = v.Value
+		} else if v.Prop == NodeMetadataPropName {
+			name = v.Value
+		} else if v.Prop == NodeMetadataDimension && v.Value == NodeMetadataPropValueTrue {
+			dim = true
+		} else if v.Prop == NodeMetadataMeasure && v.Value == NodeMetadataPropValueTrue {
+			mes = true
+		} else if v.Prop == NodeMetadataAggregationFn {
+			if _, ok := NodeMetadataAggregationFns[v.Value]; ok {
+				aggFn = v.Value
+			}
+		} else if v.Prop == NodeMetadataDataType {
+			if _, ok := NodeMetadataDataTypes[v.Value]; ok {
+				dT = v.Value
+			}
+		}
+	}
+	return interpreter.ColumnNode{
+		UID:           n.UID.String(),
+		Word:          word,
+		PUID:          n.PUID.String(),
+		Name:          name,
+		Children:      []interpreter.ValueNode{},
+		Dimension:     dim,
+		Measure:       mes,
+		AggregationFn: aggFn,
+		DataType:      dT,
+	}
 }
 
-//ColumnToNode converts the interpreter column node to node
-func ColumnToNode(c interpreter.ColumnNode) Node {
+//FromColumn converts the interpreter column node to node
+func (n Node) FromColumn(c interpreter.ColumnNode) Node {
+	metadata := []NodeMetadata{}
+	for _, v := range n.Metadata {
+		metadata = append(metadata, v)
+	}
+	for i := 0; i < len(metadata); i++ {
+		if metadata[i].Prop == NodeMetadataPropWord {
+			metadata[i].Value = c.Word
+		} else if metadata[i].Prop == NodeMetadataPropName {
+			metadata[i].Value = c.Name
+		} else if metadata[i].Prop == NodeMetadataDimension {
+			if c.Dimension {
+				metadata[i].Value = NodeMetadataPropValueTrue
+			} else {
+				metadata[i].Value = NodeMetadataPropValueFalse
+			}
+		} else if metadata[i].Prop == NodeMetadataMeasure {
+			if c.Measure {
+				metadata[i].Value = NodeMetadataPropValueTrue
+			} else {
+				metadata[i].Value = NodeMetadataPropValueFalse
+			}
+		} else if metadata[i].Prop == NodeMetadataAggregationFn {
+			if _, ok := NodeMetadataAggregationFns[c.AggregationFn]; ok {
+				metadata[i].Value = c.AggregationFn
+			} else {
+				metadata[i].Value = interpreter.AggregationFnCount
+			}
+		} else if metadata[i].Prop == NodeMetadataDataType {
+			if _, ok := NodeMetadataDataTypes[c.DataType]; ok {
+				metadata[i].Value = c.DataType
+			} else {
+				metadata[i].Value = interpreter.DataTypeString
+			}
+		}
+	}
 	return Node{}
 }
