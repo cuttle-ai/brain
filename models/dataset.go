e8642641cb06423f74431761dd2ce0bec8a9c376
Melvin Davis <melvinodsa@gmail.com>
2020-05-01 19:42:53 +0530

    added common methods

--- models/dataset.go
+++ models/dataset.go
@@ -6,6 +6,9 @@
 package models
 
 import (
+	"github.com/cuttle-ai/brain/log"
+	"github.com/cuttle-ai/octopus/interpreter"
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 )
 
@@ -53,4 +56,71 @@ type DatsetUserMapping struct {
 	UserID uint
 	//AccessType is the type of access for the user to the dataset
 	AccessType int
+}
+
+//GetColumns get the columns corresponding to a dataset
+func (d Dataset) GetColumns(conn *gorm.DB) ([]Node, error) {
+	result := []Node{}
+	err := conn.Set("gorm:auto_preload", true).Where("dataset_id = ? and type = ?", d.ID, interpreter.Column).Find(&result).Error
+	return result, err
+}
+
+//GetTable get the tables corresponding to a dataset
+func (d Dataset) GetTable(conn *gorm.DB) (Node, error) {
+	result := []Node{}
+	err := conn.Set("gorm:auto_preload", true).Where("dataset_id = ? and type = ?", d.ID, interpreter.Table).Find(&result).Error
+	if len(result) > 0 {
+		return result[0], nil
+	}
+	return Node{}, err
+}
+
+//Get will find the dataset values and set in the instance. Returns an error if couldn't find
+func (d *Dataset) Get(conn *gorm.DB) error {
+	return conn.Where("user_id = ? and id = ?", d.UserID, d.ID).Find(d).Error
+}
+
+//UpdateColumns updates the columns in the database. It will create the columns if not existing
+func (d *Dataset) UpdateColumns(l log.Log, conn *gorm.DB, cols []Node) ([]Node, error) {
+	/*
+	 * We will use the db transactions to start update
+	 * If id exists we will update
+	 * else we will create the model
+	 */
+	//starting the transaction
+	tx := conn.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if err := tx.Error; err != nil {
+		return nil, err
+	}
+
+	//will iterate through the cols for create/update
+	for i := 0; i < len(cols); i++ {
+		cols[i].DatasetID = d.ID
+		//if id doesn't exists we will create the node
+		if cols[i].ID == 0 {
+			cols[i].UID = uuid.New()
+			err := tx.Create(&cols[i]).Error
+			if err != nil {
+				l.Error("error while creating the column node for", cols[i].DatasetID, "at index", i)
+				tx.Rollback()
+				return nil, err
+			}
+			continue
+		}
+		//else we will update the node
+		for j := 0; j < len(cols[i].NodeMetadatas); j++ {
+			err := tx.Save(&(cols[i].NodeMetadatas[j])).Error
+			if err != nil {
+				l.Error("error while updating metadata of the column node for", cols[i].ID, cols[i].NodeMetadatas[j].Prop, cols[i].NodeMetadatas[j].ID)
+				tx.Rollback()
+				return nil, err
+			}
+		}
+	}
+	return cols, tx.Commit().Error
 }
